f57cf968eb377ac757d28c6355b2c4ac6236cc97
reznikomen <maksim.reznik.08@mail.ru>
2024-05-26 18:47:37 +0500

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,24 +1,79 @@
 from instr import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QLineEdit, QMessageBox, QLabel, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QRadioButton, QButtonGroup
-app = QApplication([])
+
 class FinalWin(QWidget):
-    def __init__(self):
+    def __init__(self, exp):
         super().__init__()
+        self.exp = exp
         self.set_appear()
         self.initUI()
         self.show()
+    def results(self):
+        self.index = (4*(int(self.exp.t1)+int(self.exp.t2)+int(self.exp.t3))-200)/10
+        if self.exp.age >= 15:
+            if self.index >= 15:
+                return txt_res1
+            elif self.index < 15 and self.index >= 11:
+                return txt_res2
+            elif self.index < 11 and self.index >= 6:
+                return txt_res3
+            elif self.index < 6 and self.index >= 0.5:
+                return txt_res4
+            else:
+                return txt_res5
+        elif self.exp.age >= 13 and self.age <=14:
+            if self.index >= 16.5:
+                return txt_res1
+            elif self.index < 16.5 and self.index >= 12.5:
+                return txt_res2
+            elif self.index < 12.5 and self.index >= 7.5:
+                return txt_res3
+            elif self.index < 7.5 and self.index >= 2:
+                return txt_res4
+            else:
+                return txt_res5
+        elif self.exp.age >= 11 and self.exp.age <= 12:
+            if self.index >= 18:
+                return txt_res1
+            elif self.index < 18 and self.index >= 14:
+                return txt_res2
+            elif self.index < 14 and self.index >= 9:
+                return txt_res3
+            elif self.index < 9 and self.index >= 3.5:
+                return txt_res4
+            else:
+                return txt_res5
+        elif self.exp.age >= 9 and self.exp.age <= 10:
+            if self.index >= 19.5:
+                return txt_res1
+            elif self.index < 19.5 and self.index >= 15.5:
+                return txt_res2
+            elif self.index < 15.5 and self.index >= 10.5:
+                return txt_res3
+            elif self.index < 10.5 and self.index >= 5:
+                return txt_res4
+            else:
+                return txt_res5
+        else:
+            if self.index >= 21:
+                return txt_res1
+            elif self.index < 21 and self.index >= 17:
+                return txt_res2
+            elif self.index < 17 and self.index >= 12:
+                return txt_res3
+            elif self.index < 12 and self.index >= 6.5:
+                return txt_res4
+            else:
+                return txt_res5
     def set_appear(self):
         self.setWindowTitle(txt_finalwin)
         self.move(win_x, win_y)
         self.resize(win_width,win_height)
     def initUI(self):
-        self.txt_in = QLabel(txt_index)
-        self.workhe = QLabel(txt_workheart)
+        self.workhe = QLabel(txt_workheart + self.results())
+        self.txt_in = QLabel(txt_index + str(self.index))
         self.f_line = QVBoxLayout()
         self.f_line.addWidget(self.txt_in, alignment = Qt.AlignCenter)
         self.f_line.addWidget(self.workhe, alignment = Qt.AlignCenter)
         self.setLayout(self.f_line)
-
-main_win = FinalWin()
-app.exec_()
