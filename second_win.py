2387b7f06f0eea3aaa0b832adfea6570813798c5
reznikomen <maksim.reznik.08@mail.ru>
2024-05-26 16:35:49 +0500

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,14 +1,16 @@
 from instr import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QLineEdit, QMessageBox, QLabel, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QRadioButton, QButtonGroup
-app = QApplication([])
+from final_win import *
+
 class TestWin(QWidget):
     def __init__(self):
         super().__init__()
         self.set_appear()
         self.initUI()
-        #self.connects()
+        self.connects()
         self.show()
+        
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.move(win_x, win_y)
@@ -55,8 +57,11 @@ class TestWin(QWidget):
         self.h_line.addLayout(self.r_line)
         self.setLayout(self.h_line)
     def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
-    def next_click():
+        self.next.clicked.connect(self.next_click)
+        #self.button13.clicked.connect(self.timer_test)
+    def next_click(self):
+        self.hide()
+        self.fw = FinalWin()
         self.hide(TestWin)
 
 main_win = TestWin()
